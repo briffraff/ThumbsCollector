a6489a5c4626bef93134bed63e70bfde07ca0316
borislav borisov <b_riff_raff@mail.bg>
2019-05-24 15:09:34 +0300

    Interfaces added ; Engine ; Update Excell class

---
+++ thumbsCollector/Core/Engine.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using thumbsCollector.Core.Interfaces;
+using thumbsCollector.Input;
+using thumbsCollector.Output;
+using thumbsCollector.Validations;
+
+namespace thumbsCollector.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            GlobalConstants gc = new GlobalConstants();
+
+            Console.WriteLine("........................");
+            Console.WriteLine("....THUMBS COLLECTOR....");
+            Console.WriteLine("........................");
+            Console.WriteLine();
+
+            Console.Write("SEASON :");
+            Console.WriteLine();
+
+            getInfo getSeasonalInfo = new getInfo();
+            string inputSeason = getSeasonalInfo.currentSeason();
+
+            Validator validator = new Validator(inputSeason);
+            validator.ValidateSeason();
+            string validationPattern = validator.Pattern();
+
+            var allFiles = getSeasonalInfo.AllFiles();
+            var geometryInUse = getSeasonalInfo.GeometryInUse(allFiles, validationPattern);
+
+            //main thumbs folder
+            string thumbnailsFolder = gc.thumbnailsFolder;
+
+            //enter directory where you want to save thumbs
+            Console.WriteLine();
+            Console.WriteLine("Please enter directory to save thumbs:".ToUpper());
+            string destinationPath = getSeasonalInfo.DestinationTo();
+
+            Console.WriteLine();
+            Console.WriteLine($"DO YOU WANT TO COLLECT ALL \"{inputSeason}\" THUMBNAILS? (Y/N)");
+            var isAllThumbs = getSeasonalInfo.isApproved();
+
+            string frontSide = gc.frontSide;
+            string backSide = gc.backSide;
+            string extension = gc.extension;
+            string[] allowedExtensions = gc.allowedExtensions;
+            int thumbsCopied = 0;
+            int thumbsNon = 0;
+            bool isEnd;
+
+            ////Scan thumbs folder
+            List<string> collectedPaths = getSeasonalInfo.ScanThumbsFolder(thumbnailsFolder, allowedExtensions);
+            StringBuilder badGeometries = new StringBuilder();
+
+            string inputGarment = "";
+
+            if (isAllThumbs)
+            {
+                foreach (var geometry in geometryInUse)
+                {
+
+                    //input garment code
+                    inputGarment = geometry;
+
+                    //search for end variable
+                    isEnd = inputGarment == "END!" ? true : false;
+
+                    //if input is end! then break
+                    if (isEnd == true)
+                    {
+                        break;
+                    }
+
+                    TraceCopying(inputGarment, frontSide, backSide, extension, thumbnailsFolder,
+                        destinationPath, thumbsCopied, thumbsNon, badGeometries, collectedPaths);
+                }
+
+                //print STATISTICS 
+                printAndExport.printResults(thumbsCopied, thumbsNon, badGeometries);
+                printAndExport.resultsToFile(geometryInUse, badGeometries, inputSeason);
+            }
+
+            if (isAllThumbs == false)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Insert garment code: ");
+
+                while (true)
+                {
+                    inputGarment = Console.ReadLine().ToUpper();
+
+                    //search for end variable
+                    isEnd = inputGarment == "END!" ? true : false;
+
+                    //if input is end! then break
+                    if (isEnd == true)
+                    {
+                        break;
+                    }
+
+                    TraceCopying(inputGarment, frontSide, backSide, extension, thumbnailsFolder,
+                        destinationPath, thumbsCopied, thumbsNon, badGeometries, collectedPaths);
+
+                }
+
+                //print STATISTICS 
+                printAndExport.printResults(thumbsCopied, thumbsNon, badGeometries);
+                printAndExport.resultsToFile(geometryInUse, badGeometries, inputSeason);
+            }
+
+            Console.WriteLine($"DO YOU WANT TO GENERATE A LIST OF GEOEMETRIES/SKUS USED BY {inputSeason} ?: (Y/N)");
+            var isGenerate = getSeasonalInfo.isApproved();
+
+            if (isGenerate)
+            {
+                printAndExport.createOutputFileForEndOfSeason(inputSeason);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void TraceCopying(string inputGarment, string frontSide, string backSide, string extension, string thumbnailsFolder,
+            string destinationPath, int thumbsCopied, int thumbsNon, StringBuilder badGeometries, List<string> collectedPaths
+        )
+        {
+            string garmentNameF = (inputGarment + frontSide + extension);
+            string garmentNameB = (inputGarment + backSide + extension);
+
+            //check/copy front
+            if (File.Exists(thumbnailsFolder + garmentNameF)
+                && collectedPaths.Contains(thumbnailsFolder + garmentNameF))
+            {
+                File.Copy(thumbnailsFolder + garmentNameF, destinationPath + garmentNameF, true);
+                thumbsCopied += 1;
+                Console.WriteLine("OK! - FRONT THUMB EXISTS");
+            }
+            else
+            {
+                thumbsNon += 1;
+                badGeometries.AppendLine(" " + garmentNameF);
+                Console.WriteLine("NO! - FRONT THUMB NOT EXISTS");
+
+            }
+
+            //check/copy back
+            if (File.Exists(thumbnailsFolder + garmentNameB)
+                && collectedPaths.Contains(thumbnailsFolder + garmentNameB))
+            {
+                File.Copy(thumbnailsFolder + garmentNameB, destinationPath + garmentNameB, true);
+                thumbsCopied += 1;
+                Console.WriteLine("OK! - BACK THUMB EXISTS");
+            }
+            else
+            {
+                thumbsNon += 1;
+                badGeometries.AppendLine(" " + garmentNameB);
+                Console.WriteLine("NO! - BACK THUMB NOT EXISTS");
+            }
+        }
+    }
+}
--- thumbsCollector/Excel/ExcelApp.cs
+++ thumbsCollector/Excel/ExcelApp.cs
@@ -68,28 +68,113 @@ namespace thumbsCollector.Excel
             wb.Close(true, this.wb.Name);
         }
 
+        public int Counter()
+        {
+            int counter = ws.UsedRange.Rows.Count - 2;
+            return counter;
+        }
+
+        public void RenameSheet(string season)
+        {
+            ws.Name = season;
+        }
+
+
+        public void ClearSheet()
+        {
+            ws.UsedRange.Rows.ClearContents();
+        }
+
+        public int GetSheetId()
+        {
+            int id = this.ws.Index;
+            return id;
+        }
+
+        public int WorksheetsCount()
+        {
+            return wb.Worksheets.Count;
+        }
+
+        public void AddNewWorksheet(string worksheetName)
+        {
+
+            if (ContainsSheet(worksheetName) == false)
+            {
+                ws = wb.Sheets.Add(After: wb.Sheets[wb.Sheets.Count]);
+                RenameSheet(worksheetName);
+                ws = wb.Sheets[worksheetName];
+            }
+        }
+
+        public bool ContainsSheet(string worksheetName)
+        {
+            bool found = false;
+
+            foreach (_Excel.Worksheet sheet in wb.Sheets)
+            {
+                // Check the name of the current sheet
+                if (sheet.Name == worksheetName)
+                {
+                    found = true;
+                    break; // Exit the loop now
+                }
+            }
+
+            return found;
+        }
+
+        public void SelectSheet(string inputSeason)
+        {
+            //List<Worksheet> wsList = new List<Worksheet>();
+
+            //foreach (Worksheet wbSheet in wb.Sheets)
+            //{
+            //    wsList.Add(wbSheet);
+            //}
+
+            //Worksheet wsIndex = wsList.Find(a => a.Name.Contains(inputSeason));
+
+            ws = wb.Sheets[inputSeason];
+        }
+
         public void Formatting()
         {
+            //colors
+            var white = _Excel.XlRgbColor.rgbWhite;
+            var black = _Excel.XlRgbColor.rgbBlack;
+            var gray = _Excel.XlRgbColor.rgbGray;
+            var lightGray = _Excel.XlRgbColor.rgbLightGray;
+
             //merging cells
             ws.Range["A1:C1"].Merge();
+            ws.Range["D1:F1"].Merge();
 
             //allignment
             ws.Cells.HorizontalAlignment = _Excel.XlHAlign.xlHAlignCenter;
 
-            //season cell
+            //season cell and counter cell
             //adjust font properties
-            ws.Cells[1, "A"].Font.Name = "Bahnschrift";
-            ws.Cells[1, "A"].Font.Size = 24;
-            ws.Cells[1, "A"].Font.Color = "255,0,0";
 
+            //season
+            ws.Cells[1].EntireRow.Font.Name = "Bahnschrift";
+            ws.Cells[1].EntireRow.Font.Size = 24;
+            ws.Cells[1, "A"].EntireRow.Font.Color = white;
+            ws.Rows[1].EntireRow.Interior.Color = black;
 
+            //counter
+            ws.Cells[1, "D"].Font.Color = gray;
+            ws.Columns.AutoFit();
 
             //title bar
             ws.Range["A2:C2"].Font.Name = "Bahnschrift";
             ws.Range["A2:C2"].Font.Size = 14;
             ws.Range["A2:C2"].Font.Bold = true;
             ws.Range["A2:C2"].ColumnWidth = 15;
+            ws.Rows[2].EntireRow.Interior.Color = lightGray;
 
         }
+
+
     }
 }
